8ed22633f07e47028938d6360353b607e7658cf9
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-05-24 18:24:14 -0400

    Update task-06.js

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -2,10 +2,11 @@ const input = document.getElementById('validation-input');
 const maxSymbols = document.querySelector('data-length');
 
 input.addEventListener("blur", () => {
-    if (input.length > maxSymbols || input.length < maxSymbols) {
-
+    if (input.length < 6) {
+        input.style.borderColor = "#f44336";
+        input.innerHTML;
     } else {
-        
+
     }
 });
     

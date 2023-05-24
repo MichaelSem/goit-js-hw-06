43b97ca923859f0492146498633ef1519b53c345
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-05-24 10:22:03 -0400

    Update task-06.js

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -1,10 +1,11 @@
 const input = document.getElementById('validation-input');
+const maxSymbols = document.querySelector('data-length');
 
 input.addEventListener("blur", () => {
-    if (input.length > 6 || input.length < 6) {
-        
+    if (input.length > maxSymbols || input.length < maxSymbols) {
+
     } else {
-        input.valid;
+        
     }
 });
     

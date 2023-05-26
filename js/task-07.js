fc9dcf916ce76c026034230b0293761a28db346f
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-05-25 22:19:33 -0400

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,14 +2,5 @@ const input = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
 input.addEventListener("input", () => {
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
-    console.log("hi");
     text.setAttribute("style", `font-size: ${text.style.fontSize = input.value}px;`)
 });
